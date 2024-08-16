07970f4fafdeb410784ed38f3c82464de3d34c1f
Reginaldo Batista <reginaldovlog@gmail.com>
2024-08-16 13:42:16 -0300

    Iniciando com o básico

--- java_basico/src/java_basico/Main.java
+++ java_basico/src/java_basico/Main.java
@@ -1,11 +1,28 @@
 package java_basico;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello, world!");
+		// Iniciando da melhor forma
+		System.out.println("Olá, mundo!");
 
+		for (int i = 0; i < 10; i++) {
+			System.out.printf("%d ", i + 1);
+		}
+		System.out.println();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insira um número inteiro: ");
+		int x = sc.nextInt();
+		System.out.println("Você digitou: " + x);
+		
+		String mensagem = (x >= 0) ? "é positivo" : "é negativo";
+		System.out.println(21 + " " + mensagem);
+		
+		sc.close();
 	}
 
 }
