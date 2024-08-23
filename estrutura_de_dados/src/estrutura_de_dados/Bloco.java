d41a3f55f50c2a95d38bf3392816e4d8594a55a6
Reginaldo Batista <reginaldovlog@gmail.com>
2024-08-23 20:46:17 -0300

    adicionado o arquivo 'estrutura_de_dados'

---
+++ estrutura_de_dados/src/estrutura_de_dados/Bloco.java
@@ -0,0 +1,23 @@
+package estrutura_de_dados;
+
+public class Bloco {
+
+	private int valor;
+	private Bloco proximo;
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public Bloco getProximo() {
+		return proximo;
+	}
+
+	public void setProximo(Bloco proximo) {
+		this.proximo = proximo;
+	}
+}
---
+++ estrutura_de_dados/src/estrutura_de_dados/Main.java
@@ -0,0 +1,16 @@
+package estrutura_de_dados;
+
+public class Main {
+
+	public static void main(String[] args) {
+		LinkedList inicio;
+		LinkedList a = new LinkedList();
+		a.setValor(21);
+		inicio = a;
+		
+		System.out.println(inicio.getValor());
+		
+
+	}
+
+}
