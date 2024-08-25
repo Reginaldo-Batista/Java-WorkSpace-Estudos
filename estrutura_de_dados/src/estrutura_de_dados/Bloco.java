885230fffb84081d05aa8c2170ac70c31de192f9
Reginaldo Batista <reginaldovlog@gmail.com>
2024-08-25 12:03:41 -0300

    adicionada a função de adicionar blocos no meio da lista encadeada, além de criação de menu para ver testar as funções via terminal

--- estrutura_de_dados/src/estrutura_de_dados/Bloco.java
+++ estrutura_de_dados/src/estrutura_de_dados/Bloco.java
@@ -2,36 +2,68 @@ package estrutura_de_dados;
 
 public class Bloco {
 
-	private int valor = -1;
-	private Bloco proximo = null;
+	private int valor;
+	private Bloco proximo;
+	
+	public int getValor() {
+		return this.valor;
+	}
+	
+	public Bloco() {
+		this.valor = 0;
+		this.proximo = null;
+	}
+	
+	public Bloco(int novoValor) {
+		this.valor = novoValor;
+	}
 
 	public void addBlocoInicio(int novoValor) {
 		Bloco novoBloco = new Bloco();
 		novoBloco.valor = novoValor;
 		novoBloco.proximo = this.proximo;
 		this.proximo = novoBloco;
+		this.valor++;
 	}
 
 	public void addBlocoFim(int novoValor) {
-		
+
 		if (this.proximo == null) {
 			this.addBlocoInicio(novoValor);
 		} else {
 			Bloco novoBloco = new Bloco();
 			novoBloco.valor = novoValor;
-			
+
 			Bloco aux = this.proximo;
 			while (aux.proximo != null) {
 				aux = aux.proximo;
 			}
 			aux.proximo = novoBloco;
+			this.valor++;
 		}
-		
-		
+
+	}
+
+	public void addBloco(int novoValor, int indice) {
+		if (indice <= 0 || indice > this.valor - 1) {
+			System.out.println("Posição inválida!");
+		} else {
+			Bloco novoBloco = new Bloco(novoValor);
+			Bloco auxBloco = this.proximo;
+			int auxIndice = 0;
+			while (auxIndice < indice - 1) {
+				auxBloco = auxBloco.proximo;
+				auxIndice++;
+			}
+			novoBloco.proximo = auxBloco.proximo;
+			auxBloco.proximo = novoBloco;
+			this.valor++;
+
+		}
 	}
 
 	public void imprimeBlocos() {
-		
+
 		if (this.proximo != null) {
 			Bloco aux = this.proximo;
 			while (aux != null) {
@@ -39,7 +71,7 @@ public class Bloco {
 				aux = aux.proximo;
 			}
 			System.out.println("null");
-			
+
 		} else {
 			System.out.println("Lista vazia!");
 		}
--- estrutura_de_dados/src/estrutura_de_dados/Main.java
+++ estrutura_de_dados/src/estrutura_de_dados/Main.java
@@ -1,20 +1,65 @@
 package estrutura_de_dados;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		Bloco inicio = new Bloco();
-		
-		inicio.addBlocoInicio(1);
-		inicio.addBlocoInicio(2);
-		inicio.addBlocoInicio(3);
-		inicio.addBlocoInicio(4);
-		
-		inicio.addBlocoFim(5);
-		inicio.addBlocoFim(6);
-		inicio.addBlocoFim(7);
-		
-		inicio.imprimeBlocos();
+
+		menu(inicio);
+
 	}
+
+	public static void menu(Bloco inicio) {
+
+		Scanner sc = new Scanner(System.in);
+		boolean loopMenu = true;
+		int escolha = 0;
+		int novoValor = 0;
+		int indice = 0;
+
+		do {
+
+			System.out.println("Número de blocos: " + inicio.getValor());
+			inicio.imprimeBlocos();
+			System.out.println();
+
+			System.out.println("[1] Inserir no início");
+			System.out.println("[2] Inserir no final");
+			System.out.println("[3] Inserir em um determinado índice");
+			System.out.println("[4] Sair");
+			System.out.println("Sua escolha: ");
+			escolha = sc.nextInt();
+
+			switch (escolha) {
+			case 1:
+				System.out.println("Digite o novo valor: ");
+				inicio.addBlocoInicio(sc.nextInt());
+				break;
+			case 2:
+				System.out.println("Digite o novo valor: ");
+				inicio.addBlocoFim(sc.nextInt());
+				break;
+			case 3:
+				System.out.println("Digite o novo valor: ");
+				novoValor = sc.nextInt();
+				System.out.println("Digite o índice: ");
+				indice = sc.nextInt();
+				inicio.addBloco(novoValor, indice);
+				break;
+			case 4:
+				System.out.println("Saindo...");
+				loopMenu = false;
+				break;
+			default:
+				System.out.println("Opção inválida!");
+				break;
+			}
+		} while (loopMenu);
+
+		sc.close();
+	}
+
 }
