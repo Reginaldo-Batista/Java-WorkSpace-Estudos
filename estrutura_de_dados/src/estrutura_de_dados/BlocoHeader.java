aded1af6d76a8df5a95bc0022fcbfee56d66e556
Reginaldo Batista <reginaldovlog@gmail.com>
2024-09-29 18:16:29 -0300

    criando novo projeto java: lista de exercícios 1 de estrutura de dados

---
+++ estrutura_de_dados/src/estrutura_de_dados/BlocoHeader.java
@@ -0,0 +1,24 @@
+package estrutura_de_dados;
+
+public class BlocoHeader {
+
+	private Bloco inicio;
+	private Bloco fim;
+
+	public Bloco getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Bloco inicio) {
+		this.inicio = inicio;
+	}
+
+	public Bloco getFim() {
+		return fim;
+	}
+
+	public void setFim(Bloco fim) {
+		this.fim = fim;
+	}
+
+}
---
+++ lista_exercicios1_estrutura_de_dados/src/lista_exercicios1_estrutura_de_dados/Main.java
@@ -0,0 +1,10 @@
+package lista_exercicios1_estrutura_de_dados;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("olá!");
+
+	}
+
+}
